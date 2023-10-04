86bb7baf4a57f24d3ec20daccac749ec66de5de6
Xin shen <363088847@qq.com>
2023-10-04 22:51:29 +0800

    add maxComponent

---
+++ src/compoments/MaxWidthWraper.tsx
@@ -0,0 +1,18 @@
+import { ReactNode } from "react";
+
+const MaxWidthWrapper = ({
+  children,
+  className
+}: {
+  children: ReactNode,
+  className?: string
+}) => {
+  return (
+    <div className="mx-auto w-full max-w-screen-xl px-2.5 md:px-20">
+      {children}
+    </div>
+  )
+}
+export default MaxWidthWrapper;
+
+
